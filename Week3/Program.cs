9fad589118fd1c513ef99f639b9d85171dff121d
b00981789 <mcfadden-s11@ulster.ac.uk>
2025-10-06 14:29:34 +0100

    completed basic code

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(string[] args)
+    {
+        // Create a new instance (object) of the Book class
+        // Note how the object name differs from the class name
+        Book book = new Book();
+
+        // This information is for one book in our library
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        // This is another book object.
+        // Note that the object name is 'book1' and not 'book'
+        Book book1 = new Book();
+        book1.Title = "C# Methods and classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+        // Output first book information to the console
+        Console.WriteLine($"Book title: {book.Title}");
+        Console.WriteLine($"Book Author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+        Console.WriteLine();
+
+        // Output second book information to the console
+        Console.WriteLine($"Book title: {book1.Title}");
+        Console.WriteLine($"Book Author: {book1.Author}");
+        Console.WriteLine($"Book ISBN: {book1.ISBN}");
+    }
+}
