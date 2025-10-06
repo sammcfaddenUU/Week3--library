3e2018352323bfcc226306a3f7d7ce89444873cc
b00981789 <mcfadden-s11@ulster.ac.uk>
2025-10-06 14:50:53 +0100

    Updated code with variables and properties

--- Week3/Book.cs
+++ Week3/Book.cs
@@ -2,17 +2,37 @@
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title; // private variable
+        private string author;// private variable
+        private string isbn; // private variable
 
-        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        // Title property allows access
+        // to the title private variable
+        public string Title
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
         }
 
+        // Constructor to add a new book
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+        }
+
+        // Method to display information about a book
         public void DisplayInfo()
         {
             Console.WriteLine($"Book title: {Title}");
