3729b0128fd3955e001356afeb76ea2c73572edf
b00981789 <mcfadden-s11@ulster.ac.uk>
2025-10-06 14:45:55 +0100

    Updated code with access modifiers.

---
+++ Week3/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Library
+{
+    class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,24 +1,7 @@
-﻿class Book
+﻿using Library;
+
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-
     static void Main(string[] args)
     {
         // Create a new instance (object) of the Book class
