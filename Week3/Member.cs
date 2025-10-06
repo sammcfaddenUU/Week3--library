60aee81d13cbfcfa40612b85df052a1196c3daf7
b00981789 <mcfadden-s11@ulster.ac.uk>
2025-10-06 15:03:30 +0100

    Updated code with a new Member class

---
+++ Week3/Member.cs
@@ -0,0 +1,48 @@
+﻿namespace Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        // Public properties
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Private setter makes it read-only
+        }
+        public string Name
+        {
+            get { return name; }  // get method
+            set { name = value; } // set method
+        }
+        public string Address
+        {
+            get { return address; }  // get method
+            set { address = value; } // set method
+        }
+        public int Phone
+        {
+            get { return phone; }  // get method
+            set { phone = value; } // set method
+        }
+
+        // Constructor for new member
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Library;
-
 class Program
 {
     static void Main(string[] args)
@@ -7,7 +6,19 @@ class Program
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
+        Book book1 = new Book("Ultimate C#", "Microsoft", "2233445");
+        Console.WriteLine("Currently available books");
         book.DisplayInfo();
+        book1.DisplayInfo();
+
+        // Create new instances of the Member class
+        // These new members are created using the
+        // Member constructor in the Members class
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
