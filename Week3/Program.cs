54ffdcf44eda3298aa6979c77806156dc6790837
b00981789 <mcfadden-s11@ulster.ac.uk>
2025-10-06 14:31:46 +0100

    added constructer

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -4,6 +4,16 @@
     string Author;
     string ISBN;
 
+    // Example of a constructor that allows us to 'construct' a new
+    // Book object
+    public Book(string bookTitle, string bookAuthor, string bookISBN)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+
+    // Example of a method within the Book class
     void DisplayInfo()
     {
         Console.WriteLine($"Book title: {Title}");
@@ -11,27 +21,3 @@
         Console.WriteLine($"Book ISBN: {ISBN}");
         Console.WriteLine();
     }
-
-    static void Main(string[] args)
-    {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
-        Book book = new Book();
-
-        // This information is for one book in our library
-        book.Title = "C# for beginners";
-        book.Author = "BillGates";
-        book.ISBN = "12345678";
-
-        // This is another book object.
-        // Note that the object name is 'book1' and not 'book'
-        Book book1 = new Book();
-        book1.Title = "C# Methods and classes";
-        book1.Author = "Microsoft";
-        book1.ISBN = "55667778";
-
-        // Output book information to the console
-        book.DisplayInfo();
-        book1.DisplayInfo();
-    }
-}
