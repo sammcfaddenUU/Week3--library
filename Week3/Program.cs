531cf62f556492624222b0dae94eb43f679837fe
b00981789 <mcfadden-s11@ulster.ac.uk>
2025-10-06 14:32:21 +0100

    implmented built constructor

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -4,8 +4,6 @@
     string Author;
     string ISBN;
 
-    // Example of a constructor that allows us to 'construct' a new
-    // Book object
     public Book(string bookTitle, string bookAuthor, string bookISBN)
     {
         Title = bookTitle;
@@ -13,7 +11,6 @@
         ISBN = bookISBN;
     }
 
-    // Example of a method within the Book class
     void DisplayInfo()
     {
         Console.WriteLine($"Book title: {Title}");
@@ -21,3 +18,13 @@
         Console.WriteLine($"Book ISBN: {ISBN}");
         Console.WriteLine();
     }
+
+    static void Main(string[] args)
+    {
+        // Create a new instance (object) of the Book class
+        // Note how the object name differs from the class name
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+
+        book.DisplayInfo();
+    }
+}
